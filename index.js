2df555d710bbd9c69e9f2355ee0e3413c58839f9
shererere <rafal.spizewski@gmail.com>
2018-02-12 20:22:37 +0100

    Add production config - Some line endings are broken

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ app.use('/api/v1/question', questions);
 
 app.use('/images', express.static(path.join(__dirname, 'uploads', 'images')));
 
+app.use('/', express.static(path.join(__dirname, 'dist')));
+
 app.use(auth.passport.initialize());
 
 app.post("/login", function(req, res) {
--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -3,9 +3,9 @@ import styles from './style.css';
 
 const Footer = () =>
   <footer className={styles.footer}>
-    Made by <a href="https://mateossh.me" title="Mateusz Żochowski">Mateusz Żochowski</a>
+    Made by <a href="https://mateossh.me" title="Mateusz Żochowski">Mateusz Żochowski </a>
     &amp;
-    <a href="https://shererere.me/" title="Rafał Spiżewski">Rafał Spiżewski</a>
+    <a href="https://shererere.me/" title="Rafał Spiżewski"> Rafał Spiżewski</a>
   </footer>;
 
 export default Footer;
